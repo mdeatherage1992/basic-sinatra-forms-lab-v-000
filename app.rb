94188ba9dd71ca3192395a0cbc9201bf0fc35add
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-18 12:56:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @shoot = params[:shoot]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @center = params[:center]
     erb :team
   end
 
